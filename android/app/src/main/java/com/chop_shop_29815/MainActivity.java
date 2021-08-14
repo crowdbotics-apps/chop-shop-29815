9bef93d8d57797b827f4db319cc7891a1f0e3860
Crowdbotics <team@crowdbotics.com>
2021-08-14 20:13:30 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/chop_shop_29815/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chop_shop_29815;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "chop_shop_29815";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "chop_shop_29815Identifier",
+  url: "https://chop_shop_29815Identifier.botics.co",
+  api: "https://chop_shop_29815Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
